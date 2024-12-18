2acbc5f27fd5c2bc683acd55f389ceabb30cda35
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2024-12-18 10:52:53 +0100

    update page accueil
    page accueil

---
+++ app/view/accueil.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <link rel="stylesheet" href="public\css\header.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POM - Peace of mind</title>
+</head>
+
+<body>
+    <header>
+        <ul>
+            <img src="" alt="" srcset="">
+            <li>J'ai besoin d'aide</li>
+            <li>Se renseigner</li>
+            <li>Intéragir avec nous</li>
+            <li>Qui sommes nous</li>
+        </ul>
+    </header>
+    <main>
+        <section id="banniere">
+            <div class="slogan">
+                <h1>H1 à définir</h1>
+            </div>
+        </section>
+        <section></section>
+    </main>
+</body>
+
+
+</html>
