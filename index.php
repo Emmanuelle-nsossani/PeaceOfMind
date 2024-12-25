ea27d86f092e48d3afcc3cb037f1e5c3813664d6
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2024-12-25 15:23:26 +0100

    index
    index

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+require_once 'autoload.php';
+// require_once 'config.php'; A metttre apres création db
+// require_once 'app/model/database.php'; A metttre apres création db
+
+$page = 'accueil';
+if (!empty($_GET['page'])) {
+    $page = $_GET['page'];
+}
+
+
+try {
+    switch ($page) {
+        case 'accueil':
+            $accueilController = new app\controller\AccueilController();
+            $accueilController->genererPageAccueil();
+            break;
+        default:
+            throw new Exception("Page inconnue : {$page}");
+    }
+} catch (Exception $e) {
+    echo "Erreur : " . $e->getMessage();
+}
