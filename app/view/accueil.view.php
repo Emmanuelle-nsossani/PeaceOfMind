a5816b00d621b63ce343ae398e2244534902a26d
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2024-12-25 15:11:17 +0100

    petits changement
    petits changement update

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -1,32 +1,76 @@
-<!DOCTYPE html>
-<html lang="fr">
+accueil view
 
-<head>
-    <link rel="stylesheet" href="public\css\header.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POM - Peace of mind</title>
-</head>
-
-<body>
-    <header>
-        <ul>
-            <img src="" alt="" srcset="">
-            <li>J'ai besoin d'aide</li>
-            <li>Se renseigner</li>
-            <li>Intéragir avec nous</li>
-            <li>Qui sommes nous</li>
-        </ul>
-    </header>
     <main>
         <section id="banniere">
-            <div class="slogan">
-                <h1>H1 à définir</h1>
+            <div id="container-slogan">
+                <p>L'espoir d'un nouveau départ</p>
             </div>
         </section>
-        <section></section>
+        <section>
+            <h1>Peace of Mind</h1>
+            <div id="sante-mentale">
+                <p id="">Ma santé mentale</p>
+                <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <button>Passer le test</button>
+            </div>
+            <div id="button">Je me renseigne</div>
+            <div id="button">J'ai besoin d'aide</div>
+        </section>
+        <section>
+            <h2>La santé Mentale</h2>
+            <p>Le saviez vous</p>
+            <div id="container-carte">
+                <div class="carte">
+                    <img src="" alt="" class="picto-lampe">
+                    <p>Question</p>
+                    <p>La santé mentale ne concerne que le lorem impsum ?</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h2>Notre blog présent pour vous</h2>
+            <p>Des articles sur la santé mentale</p>
+            <div class="article-container">
+                <div id="article">
+                    <p>Notre dernier article</p>
+                    <img src="" alt="">
+                    <p class="date-article">OO/OO/OO</p>
+                    <h2>H2</h2>
+                    <p>Châpo de l'article</p>
+                    <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <a href="">Lire la suite</a></p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h2>Le forum</h2>
+            <p>Un espace ou vous échangez ananymement</p>
+            <div class="message">
+                <p>Car vous n'êtes jamais le seul...</p>
+            </div>
+            <div class="message">
+                <p>Je découvre le Forum</p>
+            </div>
+        </section>
+        <section>
+            <h2>Vos témoignage</h2>
+            <p>Ceux qui ont pût bénéficier de notre aide</p>
+            <div id="container-temoignage">
+                <div></div>
+                <div>
+                    <div id="personne">
+                        <p>Anonyme</p>
+                    </div>
+                    <div>
+                        <p>Principale aide: Les appels</p>
+                        <p>Placerat enim. Quisque sollicitudin. Praesent Nullam ultrices lacus vehicula, non leo. gravida In convallis. tempor Cras eu</p>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </section>
     </main>
 </body>
 
 
 </html>
+
