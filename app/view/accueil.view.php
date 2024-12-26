20552a222c7ca5ca1c96a89061e371afdd22cff1
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2024-12-26 12:06:00 +0100

    section sante mentale
    section sante mentale

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -1,74 +1,103 @@
 <main>
-        <section id="banniere">
-            <div id="container-slogan">
-                <p>L'espoir d'un nouveau départ</p>
+    <section id="banniere">
+        <div id="container-slogan">
+            <p>L'espoir d'un nouveau départ</p>
+        </div>
+    </section>
+    <section>
+        <h1>Peace of Mind</h1>
+        <div id="sante-mentale">
+            <div>
+                <h4>Ma santé mentale</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <!-- button from Uiverse.io by Creatlydev -->
+                <button href="#" class="button">
+                    <span class="button__icon-wrapper">
+                        <svg
+                            viewBox="0 0 14 15"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="button__icon-svg"
+                            width="10">
+                            <path
+                                d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                fill="currentColor"></path>
+                        </svg>
+
+                        <svg
+                            viewBox="0 0 14 15"
+                            fill="none"
+                            width="10"
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="button__icon-svg button__icon-svg--copy">
+                            <path
+                                d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
+                                fill="currentColor"></path>
+                        </svg>
+                    </span>
+                    Faire le test
+                </button>
+
             </div>
-        </section>
-        <section>
-            <h1>Peace of Mind</h1>
-            <div id="sante-mentale">
-                <p id="">Ma santé mentale</p>
-                <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                <button>Passer le test</button>
+            <img src="public\images\logo\quiz.png" alt="">
+        </div>
+        <div id="button">Je me renseigne</div>
+        <div id="button">J'ai besoin d'aide</div>
+    </section>
+    <section>
+        <h2>La santé Mentale</h2>
+        <p>Le saviez vous</p>
+        <div id="container-carte">
+            <div class="carte">
+                <img src="" alt="" class="picto-lampe">
+                <p>Question</p>
+                <p>La santé mentale ne concerne que le lorem impsum ?</p>
             </div>
-            <div id="button">Je me renseigne</div>
-            <div id="button">J'ai besoin d'aide</div>
-        </section>
-        <section>
-            <h2>La santé Mentale</h2>
-            <p>Le saviez vous</p>
-            <div id="container-carte">
-                <div class="carte">
-                    <img src="" alt="" class="picto-lampe">
-                    <p>Question</p>
-                    <p>La santé mentale ne concerne que le lorem impsum ?</p>
+        </div>
+    </section>
+    <section>
+        <h2>Notre blog présent pour vous</h2>
+        <p>Des articles sur la santé mentale</p>
+        <div class="article-container">
+            <div id="article">
+                <p>Notre dernier article</p>
+                <img src="" alt="">
+                <p class="date-article">OO/OO/OO</p>
+                <h2>H2</h2>
+                <p>Châpo de l'article</p>
+                <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <a href="">Lire la suite</a></p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2>Le forum</h2>
+        <p>Un espace ou vous échangez ananymement</p>
+        <div class="message">
+            <p>Car vous n'êtes jamais le seul...</p>
+        </div>
+        <div class="message">
+            <p>Je découvre le Forum</p>
+        </div>
+    </section>
+    <section>
+        <h2>Vos témoignage</h2>
+        <p>Ceux qui ont pût bénéficier de notre aide</p>
+        <div id="container-temoignage">
+            <div></div>
+            <div>
+                <div id="personne">
+                    <p>Anonyme</p>
                 </div>
-            </div>
-        </section>
-        <section>
-            <h2>Notre blog présent pour vous</h2>
-            <p>Des articles sur la santé mentale</p>
-            <div class="article-container">
-                <div id="article">
-                    <p>Notre dernier article</p>
-                    <img src="" alt="">
-                    <p class="date-article">OO/OO/OO</p>
-                    <h2>H2</h2>
-                    <p>Châpo de l'article</p>
-                    <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <a href="">Lire la suite</a></p>
-                </div>
-            </div>
-        </section>
-        <section>
-            <h2>Le forum</h2>
-            <p>Un espace ou vous échangez ananymement</p>
-            <div class="message">
-                <p>Car vous n'êtes jamais le seul...</p>
-            </div>
-            <div class="message">
-                <p>Je découvre le Forum</p>
-            </div>
-        </section>
-        <section>
-            <h2>Vos témoignage</h2>
-            <p>Ceux qui ont pût bénéficier de notre aide</p>
-            <div id="container-temoignage">
-                <div></div>
                 <div>
-                    <div id="personne">
-                        <p>Anonyme</p>
-                    </div>
-                    <div>
-                        <p>Principale aide: Les appels</p>
-                        <p>Placerat enim. Quisque sollicitudin. Praesent Nullam ultrices lacus vehicula, non leo. gravida In convallis. tempor Cras eu</p>
-                    </div>
+                    <p>Principale aide: Les appels</p>
+                    <p>Placerat enim. Quisque sollicitudin. Praesent Nullam ultrices lacus vehicula, non leo. gravida In convallis. tempor Cras eu</p>
                 </div>
-                <div></div>
             </div>
-        </section>
-    </main>
+            <div></div>
+        </div>
+    </section>
+</main>
 </body>
 
 
 </html>
-
