dc560807a58b48ceba3e6b609a757f976981c180
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2024-12-29 22:23:17 +0100

    deuxieme formulaire
    deuxieme formulaire

--- app/view/connexion.view.php
+++ app/view/connexion.view.php
@@ -5,7 +5,7 @@
     </section>
     <section id="form">
         <div id="container-form">
-            <form action="">
+            <form action="" id="form-connexion">
                 <div id="title">
                     <h2>Bienvenue</h2>
                     <p>Parmis nous :)</p>
@@ -14,10 +14,27 @@
                 <input name="pseudo" type="text" placeholder="Mon pseuso" />
                 <label for="password">Mot de passe</label>
                 <input name="password" type="password" placeholder="Mot de passe" />
-                <button class="button">create</button>
+                <button class="button">Se connecter</button>
                 <div id="bottom-form">
                     <a href="">Mot de pass oublié ?</a>
                     <p>Pas de compte ? <a href="">S'inscrire</a></p>
+                </div>
+            </form>
+            <form action="" id="form-inscription">
+                <div id="title">
+                    <h2>Bienvenue</h2>
+                    <p>Rejoignez nous :)</p>
+                </div>
+                <label for="pseudo">Pseudo</label>
+                <input name="pseudo" type="text" placeholder="Mon pseuso" />
+                <label for="password">Adresse mail</label>
+                <input name="email" type="email" placeholder="email@pom.fr" />
+                <label for="password">Mot de passe</label>
+                <input name="password" type="password" placeholder="Mot de passe" />
+                <button class="button">S'inscrire</button>
+                <div id="bottom-form">
+                    <a href="">Mot de pass oublié ?</a>
+                    <p>J'ai déjà un compte, <a href="">je m'inscrie</a>.</p>
                 </div>
             </form>
         </div>
