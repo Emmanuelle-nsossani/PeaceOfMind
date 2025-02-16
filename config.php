7d3b897d8bebf3befe6db10750b784b877c2ad64
Emmanuellensossani <emmanuelle.nsossani@etu.u-pec.fr>
2025-02-16 21:05:54 +0100

    db

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// EN LOCAL
+//pour définir une constante :
+DEFINE( 'URL', 'http://localhost/sae302/');
+
+//Configuration
+DEFINE('DB_HOST', 'localhost');
+DEFINE('DB_NAME', 'peaceofmind');
+DEFINE('DB_USER', 'root');
+DEFINE('DB_PASSWORD', '');
+
+// EN LIGNE
